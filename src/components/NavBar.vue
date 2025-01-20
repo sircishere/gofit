45a6ed9d3b25125f8ad79d056d92982cb98ccc99
sircishere <christian.acevedo14@upr.edu>
2025-01-20 10:29:48 -0400

    First commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+<h1>
+    <div>MTSFitness</div>
+    <div>Icon</div>
+</h1>
+
+
+</template>
+
+<style>
+    h1{
+        display: flex;
+        justify-content: space-around;
+        font-size: 1.5rem;
+    }
+</style>
