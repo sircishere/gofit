39d6711e5259f52bc59c80a181cba60be5ba6882
sircishere <christian.acevedo14@upr.edu>
2025-02-13 22:16:12 -0400

    show logged user's name

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,25 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/NavBar.vue'
 import FormView from './views/FormView.vue'
+import { ref,onMounted } from 'vue'
+import axios from 'axios';
+
+
+const name = ref(null);
+
+onMounted( async () => {
+  //send if authenticated
+  try {
+    const response = await axios.get('http://localhost:3000/getName');
+    name.value = response.data.name; // Assign the fetched name
+  } catch (error) {
+    console.error("Error fetching name:", error);
+  }
+})
 </script>
 
 <template>
-    <Navbar/>
-
-
+  <Navbar :name="name"/>
   <RouterView />
 </template>
 
