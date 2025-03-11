e50e90e2bf648224abce25cb3416b8ac3c345df3
sircishere <eaglesirc777@gmail.com>
2025-03-11 17:04:45 -0400

    route to a different page once questionaire is completed

--- src/views/QuestionaireView.vue
+++ src/views/QuestionaireView.vue
@@ -1,44 +1,41 @@
-<script>
-import { ref } from 'vue';
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 import axios from "axios";
 
-export default {
-  data() {
-    return {
-      gender: ref(null),
-      weight: ref(null),
-      height: ref(null),
-      goal: ref(null)
-    };
-  },
-  methods: {
+const router = useRouter();
 
-    submit(height,weight,gender,goal) {
+const gender = ref(null);
+const weight = ref(null);
+const height = ref(null);
+const goal = ref(null);
 
-      axios.post("http://localhost:3000/addUserInfo",{
-          height: height,
-          weight: weight,
-          gender: gender,
-          goal: goal
-      }).then(function (response) {
-        console.log(response);
-      }).catch(function (error){
-        console.log(error);
-      });
-
-    }
-  }
+const submit = () => {
+  axios.post("http://localhost:3000/addUserInfo", {
+      height: height.value,
+      weight: weight.value,
+      gender: gender.value,
+      goal: goal.value
+  }).then((response) => {
+    console.log(response);
+    router.push("/form");
+  }).catch((error) => {
+    console.log(error);
+  });
 };
 </script>
+
 
 <template>
   <div class="flex flex-col place-items-center h-screen justify-center">
     <h1>Please input your Height</h1>
     <input class="bg-white" v-model="height" type="text">
+    <h1>Weight</h1>
     <input class="bg-white" v-model="weight" type="text">
+    <h1>Gender</h1>
     <input class="bg-white" v-model="gender" type="text">
-    <input class="bg-white" v-model="goal" type="text">
-
+    <h1>Fitness Goal</h1>
+    <input class="bg-white" v-model="goal" type="text">    
     <button @click="submit(height,weight,gender,goal)">Next</button>
   </div>
 </template>
